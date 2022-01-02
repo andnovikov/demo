5825ed09fd58e88d8eaa1379cb39c962e527137d
Andrey Novikov <andre.novikov@qiwi.com>
2022-01-02 15:34:47 +0300

    Packages refactoring

--- src/main/kotlin/ru/andnovikov/demo/DemoApplication.kt
+++ src/main/kotlin/ru/andnovikov/demo/DemoApplication.kt
@@ -1,4 +1,4 @@
-package com.example.demo
+package ru.andnovikov.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/ru/andnovikov/demo/DemoApplicationTests.kt
+++ src/test/kotlin/ru/andnovikov/demo/DemoApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.example.demo
+package ru.andnovikov.demo
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
